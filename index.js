757660330b6a320bdaa8d3783162a93e18161ccb
stevejameskent <stevejameskent1@gmail.com>
2015-04-07 22:08:33 -0400

    Use correct port and ip address

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ var server = require('http').createServer(app);
 var io = require('../..')(server);
 var port = process.env.PORT || 3000;
 
-server.listen(port, function () {
-  console.log('Server listening at port %d', port);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
 });
 
 // Routing
